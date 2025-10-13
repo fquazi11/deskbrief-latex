7d45c05410f5e1ebf8d4e3240dae6affc2c168ce
Karamjit <kka@skyplan.com>
2025-10-13 23:27:14 +0000

    Latratex update

--- src/LaraTeX.php
+++ src/LaraTeX.php
@@ -290,23 +290,10 @@ class LaraTeX
     private function generate()
     {
         $fileName = Str::random(10);
-        // Use Laravel base_path() if available, otherwise fallback to relative path from package
-        if (function_exists('base_path')) {
-            $storageRoot = base_path('storage/' . ltrim($this->tempPath, '/'));
-        } else {
-            // Fallback: go up from package src/ to project root
-            $storageRoot = realpath(__DIR__ . '/../../../../storage/' . ltrim($this->tempPath, '/'));
-            if ($storageRoot === false) {
-                $storageRoot = __DIR__ . '/../../../../storage/' . ltrim($this->tempPath, '/');
-            }
-        }
-        if (!is_dir($storageRoot)) {
-            mkdir($storageRoot, 0777, true);
-        }
-        $basetmpfname = tempnam($storageRoot, $fileName);
-        $tmpfname = preg_replace('/\.[^.\s]{3,4}$/', '', $basetmpfname);
+        $basetmpfname = tempnam(storage_path($this->tempPath), $fileName);
+        $tmpfname = preg_replace('/\\.[^.\\s]{3,4}$/', '', $basetmpfname);
         rename($basetmpfname, $tmpfname);
-        $tmpDir = $storageRoot;
+        $tmpDir = storage_path($this->tempPath);
         chmod($tmpfname, 0777);
 
         File::put($tmpfname, $this->renderedTex);
@@ -383,7 +370,7 @@ class LaraTeX
         $texFileExtension = $tmpfname . '.tex';
         $error = null;
         if (!File::exists($logFile)) {
-            throw new LaratexException("Log file does not found in " . $logFile);
+            throw new LaratexException($error,"Log file does not found.");
         }else{
             $error = File::get($logFile);
         }
